ff9f2bd633177ceac6caf44c7adefed219f002d2
Bytza <costi@Costis-iMac.local>
2016-02-03 19:18:54 +0200

    am adaugat backpage script

---
+++ Scripts/BackPage.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class BackPage : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyUp (KeyCode.Escape)) {			
+			exit();
+		}
+	}
+	void exit() {		
+		//Application.LoadLevel ("_Prefata");
+		SceneManager.LoadScene ("_Prefata");
+
+	}
+}
